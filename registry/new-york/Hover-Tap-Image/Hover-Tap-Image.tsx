98836d843843b0e549b7382e648b0f822d0418b5
Suryansh <suryanshvaish1@gmail.com>
2025-05-26 12:08:37 +0530

    Hover-Tap-Image

---
+++ registry/new-york/Hover-Tap-Image/Hover-Tap-Image.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { motion } from "framer-motion";
+
+interface style {
+  img: any;
+  width: number;
+}
+
+export default function HoverTapImage({ img, width }: style) {
+  return (
+    <>
+      <motion.img
+        whileHover={{
+          rotateX: 10,
+          rotateY: 10,
+          scale: 1.05,
+          transition: {
+            type: "spring",
+            stiffness: 300,
+            damping: 20,
+            duration: 2,
+          },
+        }}
+        whileTap={{ scale: 1.15 }}
+        style={{ perspective: 1000 }}
+        className={`cursor-pointer`}
+        src={`${img}`}
+        width={`${width}`}
+      />
+    </>
+  );
+}
